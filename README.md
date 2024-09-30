6ca61cfb16f81c27ac9d89b33434b6f53f943d5b
Luis Armando <lagonzalez@xms.cl>
2024-09-29 23:20:53 -0300

    readme.md file for the container folder

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# py-parent-folder
+# Overview
+The parent folder includes two Python projects for testing purposes, each of which can be deployed independently on the Heroku platform.
--- interfaces/IResponseStatus.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-from interfaces.IUser import IUser  # Correct import
-
-class IResponseStatus(BaseModel):
-    status: str
-    data: IUser
